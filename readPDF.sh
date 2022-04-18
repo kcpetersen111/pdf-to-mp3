bb666f3bf5aedd007f6bafb77bc44dba841f9af1
Brendan Davidson <Binary141@gmail.com>
2022-04-17 18:33:34 -0600

    Updated README to reflect functionality changes to readPDF.sh

--- readPDF.sh
+++ readPDF.sh
@@ -1,7 +1,12 @@
-echo "What is the name of the PDF file?"
-read fileName
-echo "What is the name of the output file?"
-read outputName
+if [[ $# -ne 2 ]]
+then
+	read -p "What is the name of the PDF file? " fileName
+	read -p "What is the name of the output file? " outputName
+else
+	filename=$1
+	outputName=$2
+fi
+
 pdftotext -q -raw $fileName $outputName
 
 python3 read.py $outputName
